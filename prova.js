db8078f7180a7884b5a834c7257c52b09823b5b8
Marco <92087588+Pierleoni@users.noreply.github.com>
2023-11-09 11:48:52 +0100

    fifth commit
    added new content about declare & assign variables and console.log command

---
+++ prova.js
@@ -0,0 +1,22 @@
+/*function exampleFunction() {
+    if (true) {
+        var x = 10;
+    }
+    console.log(x); // Outputs 10
+}*/
+
+
+/*const PI = 3.14;
+PI = 3.14159; // Error: Assignment to constant variable*/
+
+
+/*console.log(x); // Outputs: undefined
+var x = 5;*/
+
+
+sayHello(); // Outputs: "Hello, World!"
+
+/*function sayHello() {
+    console.log("Hello, World!");
+}*/
+
