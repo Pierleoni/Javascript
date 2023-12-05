8c0a2d939c678737c7d58439000848995d60a819
Marco <92087588+Pierleoni@users.noreply.github.com>
2023-12-05 18:44:56 +0100

    fix some ids and copy code button

--- dom.js
+++ dom.js
@@ -1,10 +1,27 @@
-function copyToClipboard() {
-    const codeSnippet = document.getElementById('codeSnippet');
-    const textArea = document.createElement('textarea');
-    textArea.value = codeSnippet.textContent;
-    document.body.appendChild(textArea);
-    textArea.select();
-    document.execCommand('copy');
-    document.body.removeChild(textArea);
+
+
+//prima prova
+function copyToClipboard(id) {
+    const codeSnippet = document.getElementById(id);
+    navigator.clipboard.writeText(codeSnippet.textContent);
     alert('Code copied to clipboard!');
 }
+
+
+
+
+
+//seconda prova 
+/*function copyToClipboard() {
+    const codeSnippet = document.getElementById('codeSnippet').textContent;
+
+    navigator.clipboard.writeText(codeSnippet)
+        .then(() => {
+            alert('Code copied to clipboard!');
+        })
+        .catch(err => {
+            console.error('Unable to Copy Code', err);
+        });
+}*/ 
+
+
