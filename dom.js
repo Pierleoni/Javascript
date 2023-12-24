526f877be8b3952c107aca22c64eae44fc727141
marco <you@example.com>
2023-12-24 16:57:43 +0100

    added icon copy clipboard instead copy code text
    to do list: add a verified icon if the code is copied, add a line to divide copy clipboard button from code snippet

--- dom.js
+++ dom.js
@@ -8,6 +8,27 @@ function copyToClipboard(id) {
 }
 
 
+/*function copyToClipboard(id) {
+    const codeSnippet = document.getElementById(id);
+    navigator.clipboard.writeText(codeSnippet.textContent)
+        .then(() => {
+            // Success: Code copied to clipboard
+            alert('Code copied to clipboard!');
+            changeIconToVerified();
+        })
+        .catch((err) => {
+            // Handle errors if any
+            console.error('Unable to copy to clipboard', err);
+        });
+}
+
+function changeIconToVerified() {
+    const copyButton = document.querySelector('.copyButton');
+    if (copyButton) {
+        // Assuming you have an element with a verified icon class or image
+        copyButton.innerHTML = '<img src="/Media/verified.png" alt="Verified Icon" class="verified-icon">';
+    }
+}*/
 
 
 
